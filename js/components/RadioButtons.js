fc6c85cc900dfd53bba3224f3e388cfae4209c67
WilliamPerezBeltran <williampbeltranprogramador@gmail.com>
2019-10-22 10:43:03 -0500

    add radio buttons

---
+++ js/components/RadioButtons.js
@@ -0,0 +1,93 @@
+import React, { Component } from 'react'
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  Dimensions
+} from 'react-native'
+
+class RadioButtons extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      value: null
+    }
+  }
+
+  render () {
+    const { options } = this.props
+    const { value } = this.state
+    return (
+      <View style={styles.buttonContainer}>
+        {options.map(item => {
+          return (
+            <View key={item.id} style={styles.button}>
+              <TouchableOpacity
+                style={styles.circle}
+                onPress={() => {
+                  this.setState({ value: item.id })
+                }}
+              >
+                {value === item.id && <View style={styles.checkedCircle} />}
+              </TouchableOpacity>
+              <Text style={styles.textButton}>{item.text} </Text>
+            </View>
+          )
+        })}
+      </View>
+    )
+  }
+}
+
+var dimension = {
+  width: Dimensions.get('window').width / 2,
+  height: Dimensions.get('window').height / 3
+}
+
+const styles = StyleSheet.create({
+  buttonContainer: {
+    borderColor: 'black',
+    borderWidth: 1,
+    flexDirection: 'row',
+    width: '100%',
+    justifyContent: 'space-between'
+  },
+  button: {
+    padding: 1,
+    width: '20%',
+    borderWidth: 1,
+    borderColor: '#ACACAC',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 10
+    // flexGrow: 0,
+    // flexShrink: 0,
+    // flexBasis: '22%'
+  },
+  circle: {
+    marginTop: 3,
+    height: 20,
+    width: 20,
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: '#ACACAC',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  checkedCircle: {
+    width: 10,
+    height: 10,
+    borderRadius: 5,
+    backgroundColor: '#737373'
+  },
+  textButton: {
+    marginTop: 5,
+    width: '100%',
+    minHeight: 40,
+    textAlign: 'center'
+  }
+})
+
+export default RadioButtons
